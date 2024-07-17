4dd1000e5e94650cbb59650485eecdf5d59eacf1
Abhijath-BS <bsabhijath@gmail.com>
2024-07-17 09:04:07 +0530

    first commit

---
+++ javascripttutorial.js
@@ -0,0 +1,61 @@
+// The script for this section is placed externally - External Script
+
+function myFunction() {
+    document.getElementById("demo4").innerHTML = "THE PARAGRAPH IS CHANGED WITH WITH A JS FILE.";
+}
+
+// The script for this section is placed externally - External Script
+
+
+let js= "amazing";
+// if (js="amazing") alert ("hello world");
+ console.log (10+20+30+40);
+
+console.log("abhi");
+
+let firstName = "abhi";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let PI =3.14;
+console.log(PI);
+
+
+let firstJob="technician";
+let secondJob="techniacl assistant";
+
+console.log(firstJob);
+
+let name="abhi";
+let age=28;
+let javascriptIsFun=true;
+
+console.log("checking the type of value of variable");
+
+console.log(typeof age);
+console.log(typeof name);
+console.log(typeof javascriptIsFun);
+
+console.log("dynamic typing example");
+
+javascriptIsFun="yes!";
+
+console.log("dynamic typing example- now javascriptIsFun is shown as string bcs of dt=ynamic typing");
+
+console.log(typeof javascriptIsFun);
+
+let danger;
+console.log(danger);
+
+console.log(typeof danger);
+
+console.log("type of null will be shown as object");
+
+console.log(typeof null);
+
+
+
+
